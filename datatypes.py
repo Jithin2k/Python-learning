0988b22b08ee4012c17afddba408eff5025e78f5
Jithin2k <jithinjmadhav@gmail.com>
2025-06-09 13:50:57 +0530

    Datatypes in Python

--- datatypes.py
+++ datatypes.py
@@ -1,8 +1,25 @@
 # Data Types in Python
-# 1.Intergers - Numbers
-# 2.String - Characters
 
-a = 5
-b = "Python"
-print(type(a))
-print(type(b))
+# 1.Numeric Types
+# int - Integer
+x = 10
+# float - Decimal Number
+pi = 3.14
+# complex = Complex Number
+z = 3 + 2j
+
+# 2.Text Type
+# str - String
+name = "Python"
+
+# 3 Boolean Type
+# bool -True/False value
+is_ok = True
+
+# Sequencs type
+#list -Order,changable
+my_list = [1,2,3]
+# tuple - Order Unchangable
+my_tuple = (1,2,3)
+# range - Sequence Of Numbers
+range(0,10)
