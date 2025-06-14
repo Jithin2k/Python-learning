43ea7f123b074ce34c7b9d2a4995f4e2604ff35f
Jithin2k <jithinjmadhav@gmail.com>
2025-06-14 21:20:27 +0530

    Ternary Operators in Python

---
+++ ternaryoperators.py
@@ -0,0 +1,15 @@
+# age = 22
+
+# if age >= 18:
+#     print("Eligible")
+# else:
+#     print("Not Eligible")    
+
+
+# Ternary Operators
+
+age = 30
+
+message = "Eligible" if age >= 18 else "Not eligible"
+
+print(message)
