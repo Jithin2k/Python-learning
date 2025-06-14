60e53a2289c0e063b04578f87c18b799afe09b9a
Jithin2k <jithinjmadhav@gmail.com>
2025-06-14 15:16:46 +0530

    Python string and methods

---
+++ strings.py
@@ -0,0 +1,19 @@
+course = "Python"
+
+# Built in functions for string
+
+# len() - returns the length of the  string
+
+# [index] - Returns the charater in the index position
+
+# [ start index : end index] - slices the string from start excluding end index
+
+
+
+print(len(course))
+
+print(course[0])  
+
+print(course[-1])  
+
+print(course[0 : 4])  
