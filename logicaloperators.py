fcce58df336a60bf935c4787564e9707f752d7ad
Jithin2k <jithinjmadhav@gmail.com>
2025-06-14 21:27:25 +0530

    Logical Operators In Python

---
+++ logicaloperators.py
@@ -0,0 +1,12 @@
+# and - Returns True if both statements are true
+# not - Returns True if at least one is true
+# or - Inverts the boolean value
+
+
+high_income = True
+good_credit = True
+
+if high_income and good_credit:
+    print("Eligible")
+else:
+    print("Not Eligible")    
