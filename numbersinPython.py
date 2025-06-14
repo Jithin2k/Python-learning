f2e1e59fda0661f1e6e9ebdefbc6b12c4b6885c2
Jithin2k <jithinjmadhav@gmail.com>
2025-06-14 19:24:10 +0530

    Numbers and its functions in python

---
+++ numbersinPython.py
@@ -0,0 +1,27 @@
+# Addition +
+# Subtraction -
+# Multiplication *
+# Division /
+ 
+
+print(10 + 3)
+
+print(10 - 3)
+
+print(10 * 3)
+
+print(10 / 3)
+
+
+print(10 // 3)
+
+
+print(10 % 3)
+
+
+print(10 ** 3)
+
+
+
+
+
